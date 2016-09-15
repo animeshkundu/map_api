57267439fe141927bc94f2a961c8ea65bbbb2a41
Agix <animesh.kundu@coupondunia.in>
2016-09-15 17:07:17 +0530

    Using port supplied by enviornment
    Signed-off-by: Agix <animesh.kundu@coupondunia.in>

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from msp_scraper_lib.base import SmartPrice
 
 # import and define tornado-y things
 from tornado.options import define
-define("port", default=5000, help="run on the given port", type=int)
+define("port", default=int(os.environ.get('PORT', 5000)), help="run on the given port", type=int)
 
 
 # application settings and handle mapping info
